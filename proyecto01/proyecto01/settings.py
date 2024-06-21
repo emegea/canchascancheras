641e3a568302855f968cea95b81841b6058acf8a
emegea <martosproject@gmail.com>
2024-06-21 16:05:21 -0300

    dejen de subir cosas a mi rama

--- proyecto01/proyecto01/settings.py
+++ proyecto01/proyecto01/settings.py
@@ -129,7 +129,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'canchascancheras',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'D10sest0d01',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
